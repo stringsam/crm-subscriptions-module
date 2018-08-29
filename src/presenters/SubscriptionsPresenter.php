0460f397d93350982ce1d34c02f52188d5538e1d
Dominik Skerhak <dominik.skerhak@gmail.com>
2018-08-29 22:45:24 +0200

    Widgetize subscriptions/new
    Moving content to widget removes dependency of SubscriptionsModule on
    SalesFunnelModule.
    
    Part of remp/crm#424
    
    remp/crm#332

--- src/presenters/SubscriptionsPresenter.php
+++ src/presenters/SubscriptionsPresenter.php
@@ -3,18 +3,9 @@
 namespace Crm\SubscriptionsModule\Presenters;
 
 use Crm\ApplicationModule\Presenters\FrontendPresenter;
-use Crm\SalesFunnelModule\Repository\SalesFunnelsRepository;
-use Nette\Application\BadRequestException;
-use Nette\Http\Request;
 
 class SubscriptionsPresenter extends FrontendPresenter
 {
-    /** @var  Request @inject */
-    public $request;
-
-    /** @var SalesFunnelsRepository @inject */
-    public $salesFunnelsRepository;
-
     public function renderMy()
     {
         $this->onlyLoggedIn();
@@ -39,31 +30,7 @@ class SubscriptionsPresenter extends FrontendPresenter
         if ($funnel === null) {
             $funnel = $this->applicationConfig->get('default_sales_funnel_url_key');
         }
-        $salesFunnel = $this->salesFunnelsRepository->findByUrlKey($funnel);
-        if (!$salesFunnel) {
-            throw new BadRequestException('invalid sales funnel urlKey: ' . $funnel);
-        }
-
-        $refererUrl = $this->request->getReferer();
-        $referer = '';
-        if ($refererUrl) {
-            $referer = $refererUrl->__toString();
-        }
-        $this->template->referer = $referer;
-        $this->template->salesFunnel = $salesFunnel->url_key;
-        $this->template->paymentGatewayId = null;
-        $this->template->subscriptionTypeId = null;
-        $this->template->utmSource = $this->getParameter('utm_source');
-        $this->template->utmMedium = $this->getParameter('utm_medium');
-        $this->template->utmCampaign = $this->getParameter('utm_campaign');
-        $this->template->utmContent = $this->getParameter('utm_content');
-        $this->template->bannerVariant = $this->getParameter('banner_variant');
-        if (isset($this->params['payment_gateway_id'])) {
-            $this->template->paymentGatewayId = intval($this->params['payment_gateway_id']);
-        }
-        if (isset($this->params['subscription_type_id'])) {
-            $this->template->subscriptionTypeId = intval($this->params['subscription_type_id']);
-        }
+        $this->template->funnel = $funnel;
 
         $showHeader = true;
         $this->template->showHeader = $showHeader;
