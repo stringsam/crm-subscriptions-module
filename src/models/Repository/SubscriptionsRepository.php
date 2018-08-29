2c24cbb56c39f0c42843f75452cbe461494c6294
Peter Dulacka <dulacka@gmail.com>
2018-08-29 22:27:46 +0200

    Removal of hardcoded subscription-content getters
    remp/crm#330

--- src/models/Repository/SubscriptionsRepository.php
+++ src/models/Repository/SubscriptionsRepository.php
@@ -222,41 +222,13 @@ class SubscriptionsRepository extends Repository
         ]);
     }
 
-    public function actualPrintSubscriptions(DateTime $date = null)
+    public function actualSubscriptionsByContentAccess(DateTime $date, string ...$contentAccess)
     {
-        if ($date == null) {
-            $date = new DateTime();
-        }
         return $this->getTable()->where([
-            'subscription_type.print' => true,
+            'subscription_type:subscription_type_content_access.content_access.name' => $contentAccess,
             'start_time <= ?' => $date,
             'end_time > ?' => $date,
-        ]);
-    }
-
-    public function actualFridayPrintSubscriptions(DateTime $date = null)
-    {
-        if ($date == null) {
-            $date = new DateTime();
-        }
-        return $this->getTable()->where([
-            'subscription_type.print_friday' => true,
-            'start_time <= ?' => $date,
-            'end_time > ?' => $date,
-        ]);
-    }
-
-    public function actualClubPrintSubscriptions(DateTime $date = null)
-    {
-        if ($date == null) {
-            $date = new DateTime();
-        }
-
-        return $this->getTable()->where([
-            'subscription_type.club' => true,
-            'start_time <= ?' => $date,
-            'end_time > ?' => $date,
-        ]);
+        ])->group('user_id');
     }
 
     public function createdOrModifiedSubscriptions(DateTime $fromTime, DateTime $toTime)
@@ -286,20 +258,11 @@ class SubscriptionsRepository extends Repository
 //          ->where('end_time > ?', $date->format(DateTime::ATOM));
     }
 
-    public function printSubscriptions()
+    public function subscriptionsByContentAccess(string ...$contentAccess)
     {
-        return $this->getTable()->where(['subscription_type.print = ? OR subscription_type.print_friday = ?' => [1, 1]])->group('user_id');
-    }
-
-    // todo by som navrhol premenovat, je tam napevno 10:00:00
-    // cize to ma nejaky bisniz ucel, a z nazvu funkcie to neni velmi jasne
-    public function actualUserMobileSubscriptions($userId)
-    {
-        $now = new DateTime();
-        return $this->getTable()->where('subscription_type.mobile', true)
-            ->where('user_id', $userId)
-            ->where('start_time <= ?', $now->format('Y-m-d 10:00:00'))
-            ->where('end_time > ?', $now->format('Y-m-d 10:00:00'));
+        return $this->getTable()->where([
+            'subscription_type:subscription_type_content_access.content_access.name' => $contentAccess,
+        ])->group('user_id');
     }
 
     public function subscriptionsEndBetween(DateTime $endTimeFrom, DateTime $endTimeTo, $withNextSubscription = null)
