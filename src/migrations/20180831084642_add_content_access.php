8998fbdf45c15e5b8cdb60f288cad6fddae5e87c
Dominik Skerhak <dominik.skerhak@gmail.com>
2018-08-31 21:51:39 +0200

    Add module specific migrations
    - PhinxRegistrator updated to load migrations from each extension.
      Heavily inspired by ab370c1fdc1d00aa7960cb53cd468e63f4b19236 (part of
      MR remp/crm!257). Thank you Miro!
    
    - Existing migrations won't be used by CRM skeleton. We are initializing
      new DB with current state of tables for each extension. Old migrations
      will be probably thrown away.
    
    - There are still foreign keys between tables of different extensions.
      Since all these modules are (at least for now) mandatory, I leave them
      as they are. We can revisit later.
    
    - Order of migrations is decided by version (datetime) stamp.
    
    Part of remp/crm#424.
    
    remp/crm#331
    
    Co-Authored-By: Miroslav Svitok <mirosvitok@gmail.com>

---
+++ src/migrations/20180831084642_add_content_access.php
@@ -0,0 +1,56 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddContentAccess extends AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<SQL
+SET NAMES utf8mb4;
+SET time_zone = '+00:00';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+
+CREATE TABLE IF NOT EXISTS `content_access` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `class` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sorting` int(11) NOT NULL DEFAULT '100',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `sorting` (`sorting`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `subscription_type_content_access` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subscription_type_id` int(11) NOT NULL,
+  `content_access_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `subscription_type_id` (`subscription_type_id`),
+  KEY `content_access_id` (`content_access_id`),
+  CONSTRAINT `subscription_type_content_access_ibfk_1` FOREIGN KEY (`subscription_type_id`) REFERENCES `subscription_types` (`id`),
+  CONSTRAINT `subscription_type_content_access_ibfk_2` FOREIGN KEY (`content_access_id`) REFERENCES `content_access` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO content_access (name,description,class,sorting,created_at,updated_at) VALUES
+('web', 'Web access', 'label label-primary', 100, NOW(), NOW()),
+('print', 'Print', 'label label-info', 200, NOW(), NOW()),
+('mobile', 'Mobile access', 'label label-warning', 300, NOW(), NOW());
+
+-- 2018-08-31 07:42:42
+SQL;
+        $this->execute($sql);
+    }
+
+    public function down()
+    {
+        // TODO: [refactoring] add down migrations for module init migrations
+        $this->output->writeln('Down migration is not available.');
+    }
+}
