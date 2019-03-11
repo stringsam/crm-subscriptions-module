6980488c825139b961936914e192c04073c9a8dc
Tomas Majer <tomasmajer@gmail.com>
2019-03-11 13:42:43 +0100

    Payment items
    Refactor work with payment_items. Move to separated classes with interface and each module could export their own paymentitem.

--- src/models/PaymentItem/SubscriptionTypePaymentItem.php
+++ src/models/PaymentItem/SubscriptionTypePaymentItem.php
@@ -9,14 +9,37 @@ class SubscriptionTypePaymentItem implements PaymentItemInterface
 {
     const TYPE = 'subscription_type';
 
-    private $subscriptionType;
+    private $subscriptionTypeItem;
 
     private $count;
 
-    public function __construct(IRow $subscriptionType, int $count = 1)
-    {
-        $this->subscriptionType = $subscriptionType;
+    private $name = null;
+
+    private $vat = null;
+
+    private $price = null;
+
+    public function __construct(
+        IRow $subscriptionTypeItem,
+        int $count = 1,
+        $name = null,
+        float $price = null,
+        int $vat = null
+    ) {
+        $this->subscriptionTypeItem = $subscriptionTypeItem;
         $this->count = $count;
+        $this->name = $name;
+        $this->price = $price;
+        $this->vat = $vat;
+    }
+
+    public static function fromSubscriptionType(IRow $subscriptionType)
+    {
+        $rows = [];
+        foreach ($subscriptionType->related('subscription_type_items')->order('sorting') as $item) {
+            $rows[] = new SubscriptionTypePaymentItem($item);
+        }
+        return $rows;
     }
 
     public function type(): string
@@ -26,17 +49,26 @@ class SubscriptionTypePaymentItem implements PaymentItemInterface
 
     public function name(): string
     {
-        return $this->subscriptionType->user_label ?? $this->subscriptionType->name;
+        if ($this->name) {
+            return $this->name;
+        }
+        return $this->subscriptionTypeItem->name;
     }
 
     public function price(): float
     {
-        return $this->subscriptionType->price;
+        if ($this->price !== null) {
+            return $this->price;
+        }
+        return $this->subscriptionTypeItem->amount;
     }
 
     public function vat(): int
     {
-        return 20; // TODO
+        if ($this->vat !== null) {
+            return $this->vat;
+        }
+        return $this->subscriptionTypeItem->vat;
     }
 
     public function count(): int
@@ -47,7 +79,25 @@ class SubscriptionTypePaymentItem implements PaymentItemInterface
     public function data(): array
     {
         return [
-            'subscription_type_id' => $this->subscriptionType->id,
+            'subscription_type_id' => $this->subscriptionTypeItem->subscription_type_id,
         ];
+    }
+
+    public function forceName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function forceVat(int $vat): self
+    {
+        $this->vat = $vat;
+        return $this;
+    }
+
+    public function forcePrice(float $price): self
+    {
+        $this->price = $price;
+        return $this;
     }
 }
