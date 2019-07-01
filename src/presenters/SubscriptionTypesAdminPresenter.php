2c1ad47b6d0c5e39f5328acac2ec38578d4d955b
Adam Žoldák <zoldakadam@gmail.com>
2019-07-01 16:28:19 +0200

    refactor add/edit subscription types to use modal form
    remp/crm#440

--- src/presenters/SubscriptionTypesAdminPresenter.php
+++ src/presenters/SubscriptionTypesAdminPresenter.php
@@ -90,6 +90,16 @@ class SubscriptionTypesAdminPresenter extends AdminPresenter
         $this->template->meta = $this->subscriptionTypesMetaRepository->subscriptionTypeMeta($subscriptionType);
     }
 
+    protected function createComponentSubscriptionTypeItemsForm()
+    {
+        $form = $this->subscriptionTypeItemsFormFactory->create($this->params['id']);
+        $this->subscriptionTypeItemsFormFactory->onSave = function ($subscriptionTypeItem) {
+            $this->flashMessage($this->translator->translate('subscriptions.admin.subscription_types.messages.subscription_type_item_created'));
+            $this->redirect('SubscriptionTypesAdmin:Show', $subscriptionTypeItem->subscription_type_id);
+        };
+        return $form;
+    }
+
     public function handleRemoveSubscriptionTypeItem($itemId)
     {
         $item = $this->subscriptionTypeItemsRepository->find($itemId);
